cdd83782ebe56383ced1c1af3bb8c89e1acb8f67
SerjMan <Manuylov_serj@ukr.net>
2024-06-19 21:34:00 +0300

    apdeted

--- src/app.mjs
+++ src/app.mjs
@@ -12,10 +12,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 app.engine('ejs', ejs.renderFile)
 
